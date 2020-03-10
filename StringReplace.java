13468c70509c905f9e2c576e56bb9bb2fcd41c61
priyagarad <nirmalagarad1526@gmail.com>
2020-03-10 17:49:38 +0530

    second file

---
+++ StringReplace.java
@@ -0,0 +1,19 @@
+package felloshipProgram;
+
+import java.util.Scanner;
+
+public class StringReplace {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("enter the string");
+	    String str=sc.nextLine();
+	    
+	    String newString="Hello"+" "+str+" "+" How are you"; 
+	    System.out.println(newString);
+	}
+
+}
